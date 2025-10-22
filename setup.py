4e0c9b01ce81f07bcb751b4101f5a7e4a6782801
uPiscium <upiscium@gmail.com>
2025-10-22 23:27:39 +0900

    Update setting

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="pylognet",
     version="0.1.0",
-    packages=find_packages(include=["src*"]),
+    packages=find_packages(include=["pylognet*"]),
     install_requires=[],
     include_package_data=True,
 )
--- test_server.py
+++
@@ -1,6 +0,0 @@
-from src import server
-
-if __name__ == "__main__":
-    # Start the logging server
-    server = server.LoggingService()
-    server.run()
