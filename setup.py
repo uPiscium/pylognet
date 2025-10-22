702356321561df16092f3a8907fc9041426acff0
uPiscium <upiscium@gmail.com>
2025-10-22 23:24:49 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
 setup(
-    packages=find_packages(),
-    version="0.1",
     name="pylognet",
+    version="0.1.0",
+    packages=find_packages(include=["src*"]),
+    install_requires=[],
     include_package_data=True,
-    package_dir={"": "./src"},
 )
